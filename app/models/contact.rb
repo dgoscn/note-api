e3a11c8d647e09ed55c9d0a730756de57ea3f3fc
dgoscn <diego-ii@live.com>
2019-11-13 13:07:35 -0300

    Adding belongs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ApplicationRecord
+belongs_to :kind, option: true
+
+  # def author
+  #   "Irineu"
+  # end
+
+  # def kind_description
+  #   self.kind.description
+  # end
+
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:author, :kind_description]
+  #   )
+  # end
 end
